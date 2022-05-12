767a81fda6269c32ed20c869530aa12d7197a7b8
ilyas6773 <mister.tolegenov@yandex.ru>
2022-05-12 23:03:18 +0600

    Added test.py for testing requests on client side

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+from cgi import test
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return{"HelloWorld!"}
+video_put_args = reqparse.RequestParser()
 
-api.add_resource(HelloWorld,"/helloworld")
+
+videos = {
+}
+
+
+class Video(Resource):
+    def get(self,video_id):
+        return videos[video_id]
+
+    def put(self,video_id):
+        return {}
+
+api.add_resource(Video,"/video/<int:video_id>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
