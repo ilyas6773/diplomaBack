37319a1acf2b469db39fee6efe62152b43967be2
ilyas6773 <mister.tolegenov@yandex.ru>
2022-05-19 19:07:10 +0600

    some things to add

--- main.py
+++ main.py
@@ -1,25 +1,81 @@
-from cgi import test
+from unittest import result
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:6235@localhost/houses'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['JSON_AS_ASCII'] = False
 
-video_put_args = reqparse.RequestParser()
+db = SQLAlchemy(app)
+
+db.create_all()
 
 
-videos = {
+class HouseModel(db.Model):
+	__tablename__ = 'house_model'
+	column1 = db.Column(db.Integer, primary_key=True)
+	address = db.Column(db.String(100))
+	price = db.Column(db.String(100))
+	city = db.Column(db.String(100))
+	title = db.Column(db.String(100))
+	isbalconyglazed = db.Column(db.String(100))
+	ceiling = db.Column(db.String(100))
+	flatrenovation = db.Column(db.String(100))
+	housetype = db.Column(db.String(100))
+	year = db.Column(db.String(100))
+	balcony = db.Column(db.String(100))
+	bathroom = db.Column(db.String(100))
+	residentialcomplex = db.Column(db.String(100))
+	floor = db.Column(db.String(100))
+	totalfloor = db.Column(db.String(100))
+	area = db.Column(db.String(100))
+    #realtor = db.Column(db.String(100))
+	longitude = db.Column(db.String(100))
+	latitude = db.Column(db.String(100))
+
+	def __repr__(self):
+		return f"{column1},{address},{price},{city},{title},{isbalconyglazed},{ceiling},{flatrenovation},{housetype},{year},{balcony},{bathroom},{residentialcomplex},{floor},{totalfloor},{area},{longitude},{latitude})"
+
+resource_fields = {
+	'column1': fields.Integer,
+	'address': fields.String,
+	'price': fields.String,
+	'city': fields.String,
+	'title': fields.String,
+	'isbalconyglazed': fields.String,
+	'ceiling': fields.String,
+	'flatrenovation': fields.String,
+	'housetype': fields.String,
+	'year': fields.String,
+	'bathroom': fields.String,
+	'residentialcomplex': fields.String,
+	'floor': fields.String,
+	'totalfloor': fields.String,
+	'area': fields.String,
+	#'realtor': fields.String,
+	'longitude': fields.String,
+	'latitude': fields.String
 }
 
+class House(Resource):
+	@marshal_with(resource_fields)
+	def get(self, house_id):
+		result = HouseModel.query.filter_by(column1=house_id).first()
+		if not result:
+			abort(404, message="Could not find video with that id")
+		print("-----------------")
+		print(result.column1)
+		return result
 
-class Video(Resource):
-    def get(self,video_id):
-        return videos[video_id]
 
-    def put(self,video_id):
-        return {}
 
-api.add_resource(Video,"/video/<int:video_id>")
+
+api.add_resource(House, "/house/<int:house_id>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
+db.create_all()
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+from urllib import response
 import requests
+import json
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:5000"
 
-response=requests.put(BASE+"video/1",{"likes":10 })
+response = requests.get(BASE + "/house/1")
 print(response.json())
